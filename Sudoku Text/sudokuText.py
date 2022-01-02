62b9bde4c05bb00d04b751b993696df46928f8b8
Lotus Wei <78193694+WeiLotus@users.noreply.github.com>
2022-01-02 01:18:11 -0800

    Update sudokuText.py

--- Sudoku Text/sudokuText.py
+++ Sudoku Text/sudokuText.py
@@ -9,8 +9,6 @@ board = [[0,0,0,0,0,0,3,9,0],[8,7,0,0,3,2,0,5,0],[0,3,0,0,0,4,1,0,2],
          [7,1,0,0,2,9,0,0,8],[0,2,4,0,0,0,9,7,0],[6,0,0,3,4,0,0,1,5],
          [5,0,7,4,0,0,0,2,0],[0,6,0,8,7,0,0,4,9],[0,4,8,0,0,0,0,0,0]]
 
-#print(board)
-
 def solver(bo):
 
     # find empty cells
@@ -20,7 +18,6 @@ def solver(bo):
                 if bo[row][col] == 0:
                    emp = (row,col)
                    return emp
-                    #print(row,col)
     
     cell = empty_find(bo)
     if cell:
@@ -38,9 +35,6 @@ def solver(bo):
           bo[r][c] = 0
     
     return False
-    
-
-
 # end of solver method
  
 
@@ -82,15 +76,6 @@ def show_board(board):
                 print(str(board[i][j]), end = " ")
     print("- - - - - - - - - - -")
 
-grid = [ [3, 0, 6, 5, 0, 8, 4, 0, 0], 
-         [5, 2, 0, 0, 0, 0, 0, 0, 0], 
-         [0, 8, 7, 0, 0, 0, 0, 3, 1], 
-         [0, 0, 3, 0, 1, 0, 0, 8, 0], 
-         [9, 0, 0, 8, 6, 3, 0, 0, 5], 
-         [0, 5, 0, 0, 9, 0, 6, 0, 0], 
-         [1, 3, 0, 0, 0, 0, 2, 5, 0], 
-         [0, 0, 0, 0, 0, 0, 0, 7, 4], 
-         [0, 0, 5, 2, 0, 6, 3, 0, 0] ]
 
 solver(board)
 show_board(board)
