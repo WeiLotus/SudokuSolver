ad3adc2b7ec588b90d34ee9a38ead6971630e0a6
lotus.wei <lotuswei@outlook.com>
2022-01-02 01:09:25 -0800

    complete main

--- Sudoku Text/sudokuText.py
+++ Sudoku Text/sudokuText.py
@@ -4,6 +4,46 @@
 # Or if the board is unsolvable, throw an error. 
 # Made in collaboration of Johnson Chen and Lotus Wei
 
+# create a sudoku board
+board = [[0,0,0,0,0,0,3,9,0],[8,7,0,0,3,2,0,5,0],[0,3,0,0,0,4,1,0,2],
+         [7,1,0,0,2,9,0,0,8],[0,2,4,0,0,0,9,7,0],[6,0,0,3,4,0,0,1,5],
+         [5,0,7,4,0,0,0,2,0],[0,6,0,8,7,0,0,4,9],[0,4,8,0,0,0,0,0,0]]
+
+#print(board)
+
+def solver(bo):
+
+    # find empty cells
+    def empty_find(bo):
+        for row in range(9):
+            for col in range(9):
+                if bo[row][col] == 0:
+                   emp = (row,col)
+                   return emp
+                    #print(row,col)
+    
+    cell = empty_find(bo)
+    if cell:
+        r,c = (cell[0],cell[1])
+    else:
+        return True
+        
+    for i in range(1,10):    
+        if valid(bo, emp, i):
+          bo[r][c] = i  
+              
+          if solver(bo):
+              return True
+          
+          bo[r][c] = 0
+    
+    return False
+    
+
+
+# end of solver method
+ 
+solver(board)
 
 # Effects: returns if the attempted input number on the corresponding position is valid
 # Param:
